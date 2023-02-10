16c156555ca2e81a6caddf26a470e2fbeb7fbbe2
nartnattakrivichian <nartnatta.k@ku.th>
2023-02-10 11:31:03 +0700

    card done

--- kk-frontend/src/App.jsx
+++ kk-frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Card from './components/Card'
 function App() {
 
   return (
-    <Card />
+    <Card name="Bedroom" mode="Manual" pet_status="true" />
   )
 }
 
--- kk-frontend/src/components/Card.jsx
+++ kk-frontend/src/components/Card.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import '../styles/Card.css'
 
-const Card = () => {
-  return (
-    <div>Card</div>
-  )
+const Card = ({name, mode, pet_status}) => {
+    const icon = () => {
+        if (name == "Kitchen") {
+            return "https://cdn-icons-png.flaticon.com/512/3474/3474313.png"
+        } 
+        else if (name == "Living room") {
+            return "https://icons.iconarchive.com/icons/icons8/ios7/512/Household-Livingroom-icon.png"
+        }
+        else if (name == "Bedroom") {
+            return "https://uxwing.com/wp-content/themes/uxwing/download/household-and-furniture/double-bed-icon.png"
+        }
+    }
+
+
+    if (pet_status == 'true') {
+        return (
+            <div className='container'>
+                <div className='item' id='icon'><img src={icon()} alt="room icon" /></div>
+                <div className='item' id='info'>
+                    <h3>{name}</h3>
+                    <p>{mode} mode</p>
+                </div>
+                <div className='status'><img src="https://i.imgur.com/N34i12t.png" alt="" /></div>
+            </div>
+        )
+    } 
+    else {
+        return (
+            <div className='container'>
+                <div className='item' id='icon'><img src={icon()} alt="room icon" /></div>
+                <div className='item' id='info'>
+                    <h3>{name}</h3>
+                    <p>{mode} mode</p>
+                </div>
+            </div>
+        )
+    }
+  
 }
 
 export default Card
